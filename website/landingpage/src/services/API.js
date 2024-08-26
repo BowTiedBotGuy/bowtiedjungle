0cb59bc85047cbcfa7d0ed02f4af08cc975c2755
BowTiedBotGuy <mackw2019@gmail.com>
2024-08-26 14:49:52 -0400

    Not having auth in the front end

--- website/landingpage/src/services/API.js
+++ website/landingpage/src/services/API.js
@@ -1,165 +1,56 @@
 import axios from 'axios';
-import { Auth } from 'aws-amplify';
 
+const API_BASE_URL = `https://${process.env.REACT_APP_DOMAIN_SUFFIX}.bowtiedlist.com/`;
 
-const getClient = async (clientIdOverride, useCache) => {
-
-  // const clientID = clientIdOverride || sessionStorage.getItem('selectedClient');
-  const JWT =  await Auth.currentSession().then(data => data.getIdToken().getJwtToken());
-  const identityToken = await Auth.currentCredentials();
-  const cache = await caches.open(`bowtiedlist`);
-
-  const options = {
-    baseURL: `https://${process.env.REACT_APP_DOMAIN_SUFFIX}.bowtiedlist.com/`,
-    timeout: 900000,
-    headers: {
-      // 'Accept-Encoding':'gzip, deflate, br', //doesn't seem to be necessary? also chrome yells at me for trying to do it
-      'Content-Type': 'application/json;charset=utf-8',
-      // 'client-id': clientID,
-      "Authorization": JWT,
-      "key": identityToken.accessKeyId,
-      "secret": identityToken.secretAccessKey,
-      "session": identityToken.sessionToken
-    }
-  };
-
-  const client = axios.create(options);
-
-  if (useCache) {
-
-    const requestHandler = async (request) => {
-      if (request.method === 'GET' || 'get') {
-        const response = await cache.match(request.baseURL + request.url)
-        if (response) {
-          request.headers.cached = true;
-          request.data = await response.json();
-          return Promise.reject(request);
-        } else {
-          return request;
-        }
-      } else {
-        return request;
-      }
-    };
-  
-    // Add a request interceptor
-    client.interceptors.request.use(
-      requestConfig => requestHandler(requestConfig),
-      (requestError) => {
-        // this is where we could universally log request errors
-        console.log('Request error: ', requestError);
-        return Promise.reject(requestError);
-      },
-    );
-  
-    // Add a response interceptor
-    client.interceptors.response.use(
-      (response) => {
-        if ((response.config?.method === 'GET' || 'get') && [200,201,202,203,204,205].includes(response.status)) {
-          const now = Date.now();
-          response.headers.timeCached = new Date(now).toLocaleString();
-          cache.put(response.config.baseURL + response.config.url, new Response(JSON.stringify(response.data), response));
-        }
-        return response;
-      },
-      (error) => {
-        if (error.headers?.cached === true) {
-          return Promise.resolve(error);
-        }
-        return Promise.reject(error);
-      }
-    );
-  
-    return client;
-
-  } else { //below is what we do for all requests where we don't explicitly say useCache
-
-    // Add a request interceptor
-    client.interceptors.request.use(
-      requestConfig => requestConfig,
-      (requestError) => {
-        // this is where we could universally log request errors
-        console.log('Request error: ', requestError);
-
-        return Promise.reject(requestError);
-      },
-    );
-
-    // Add a response interceptor
-    client.interceptors.response.use(
-      async (response) => {
-        const isCached = await cache.match(response.request?.responseURL);
-        if (!!isCached && (response.config?.method === 'GET' || 'get') && [200,201,202,203,204,205].includes(response.status)) {
-          const now = Date.now();
-          response.headers.timeCached = new Date(now).toLocaleString();
-          cache.put(response.config.baseURL + response.config.url, new Response(JSON.stringify(response.data), response));
-        }
-        return response;
-      },
-      (error) => {
-        if (error?.response?.status >= 400) {
-          // this is where we could universally log response errors
-          console.log('Response error: ', error);
-          console.log('response when error: ', error?.response?.data?.body);
-        }
-
-        return Promise.reject(error.response?.data);
-      },
-    );
-
-    return client;
+const client = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 900000, // Adjust timeout as needed
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8'
   }
-};
-
+});
 
 const API = {
-  async get(url, { conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.get(url, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  get(url, conf = {}) {
+    return client.get(url, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async delete(url, { conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.delete(url, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  delete(url, conf = {}) {
+    return client.delete(url, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async head(url, { conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.head(url, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  head(url, conf = {}) {
+    return client.head(url, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async options(url, { conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.options(url, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  options(url, conf = {}) {
+    return client.options(url, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async post(url, { data = {}, conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.post(url, data, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  post(url, data = {}, conf = {}) {
+    return client.post(url, data, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async put(url, { data = {}, conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.put(url, data, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  put(url, data = {}, conf = {}) {
+    return client.put(url, data, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 
-  async patch(url, { data = {}, conf = {}, clientId = null, useCache = false } = {}) {
-    const cli = await getClient(clientId, useCache);
-    return cli.patch(url, data, conf)
-      .then(response => Promise.resolve(response))
-      .catch(error => Promise.reject(error));
+  patch(url, data = {}, conf = {}) {
+    return client.patch(url, data, conf)
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data));
   },
 };
 
