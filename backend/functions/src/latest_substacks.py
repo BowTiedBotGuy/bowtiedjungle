55c3f2d833250aa863c72f7f8f9560b49b9e3019
BowTiedBotGuy <mackw2019@gmail.com>
2024-08-26 15:18:04 -0400

    adding in some error handling

--- backend/functions/src/latest_substacks.py
+++ backend/functions/src/latest_substacks.py
@@ -8,13 +8,24 @@ def lambda_handler(event,context):
     rawrss = ['https://bowtiedbull.substack.com/feed']
 
     posts = []
+    try:
+        # dict_keys(['bozo', 'entries', 'feed', 'headers', 'etag', 'href', 'status', 'encoding', 'version', 'namespaces'])
+        for url in rawrss:
+            feed = feedparser.parse(url)
+            for post in feed.entries:
+                posts.append((post.title, post.link, post.summary, post.summary_detail, post.content, post.published))
 
-    # dict_keys(['bozo', 'entries', 'feed', 'headers', 'etag', 'href', 'status', 'encoding', 'version', 'namespaces'])
-    for url in rawrss:
-        feed = feedparser.parse(url)
-        for post in feed.entries:
-            posts.append((post.title, post.link, post.summary, post.summary_detail, post.content, post.published))
+        
+        return {
+                'statusCode': 200,
+                'data': posts
+            }
+    except Exception as e:
+        # An error occurred during processing
+        return {
+            'statusCode': 500,
+            'error': str(e)
+        }
 
-    
-    return posts
+
 
