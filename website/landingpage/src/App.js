06557b1e4c9b8e5eeb92540052930fbf7e8c7222
BowTiedBotGuy <mackw2019@gmail.com>
2024-08-26 09:43:48 -0400

    no need for ec2 / telegram now

--- website/landingpage/src/App.js
+++ website/landingpage/src/App.js
@@ -3,9 +3,8 @@ import Navbar from './components/NavBar';
 import Hero from './components/Hero';
 import Card from './components/Card';
 
-const cities = [
+const members = [
   { name: "BowTiedBull", image: "https://substackcdn.com/image/fetch/w_96,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fbucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com%2Fpublic%2Fimages%2F60b51132-7e25-4ce8-ad9c-fe7fae698621_140x140.png", description: "BowTiedBull" },
-  // { name: "Berlin", image: "url-to-image", description: "Vibrant nightlife and deep history." },
   // Add more members as needed
 ];
 
@@ -15,7 +14,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className="container mx-auto flex flex-wrap justify-center">
-        {cities.map(member => <Card key={member.name} member={member} />)}
+        {members.map(member => <Card key={member.name} member={member} />)}
       </div>
     </div>
   );
