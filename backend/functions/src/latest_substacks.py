78ff98f1fb52cad01d402370fd8b59f497a5d26e
BowTiedBotGuy <mackw2019@gmail.com>
2024-08-26 22:00:50 -0400

    Adding in the ability to query existing substacks

--- backend/functions/src/latest_substacks.py
+++ backend/functions/src/latest_substacks.py
@@ -1,11 +1,47 @@
 import feedparser
+import os
 from lambda_decorators import json_http_resp, cors_headers, load_json_body
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.exc import SQLAlchemyError
+from dotenv import load_dotenv
+
+def fetch_data(session_factory):
+    # Directly use the session factory to control the session lifecycle
+    session = session_factory()
+    try:
+        result = session.execute(text("SELECT DISTINCT substack FROM bowtiedlist WHERE substack IS NOT NULL"))
+        urls = [url[0] for url in result.fetchall()]
+        return urls
+    except SQLAlchemyError as e:
+        print(f"Error fetching Substack URLs: {str(e)}")
+        return []
+    finally:
+        session.close()  # Properly close the session
+
 
 @cors_headers
 @load_json_body
 @json_http_resp
 def lambda_handler(event,context):
-    rawrss = ['https://bowtiedbull.substack.com/feed']
+    load_dotenv()
+    # Retrieve database credentials and AWS S3 credentials from environment variables
+    user = os.getenv('db_username')
+    host = os.getenv('db_host')
+    password = os.getenv('db_password')
+    database = os.getenv('db_database')
+    
+    # Create the database engine
+    engine = create_engine(f"postgresql://{user}:{password}@{host}/{database}", echo=True)
+    
+    # Create a configured "Session" class
+    session_factory = scoped_session(sessionmaker(bind=engine))
+    
+    # Fetch URLs using the session factory
+    urls = fetch_data(session_factory=session_factory)
+    session_factory.remove()  # Correct usage of remove to clear any residual session after use
+
+    rawrss = [url + '/feed' for url in urls]
 
     posts = []
     try:
@@ -28,4 +64,5 @@ def lambda_handler(event,context):
 
 
 if __name__ == '__main__':
-    lambda_handler({},{})
+    data = lambda_handler({},{})
+    print(data)
