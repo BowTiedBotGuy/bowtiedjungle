9175e19540d739c0051f9d831e1a28ec2dd10992
BowTiedBotGuy <mackw2019@gmail.com>
2024-08-28 08:43:17 -0500

    making hero smaller on mobile

--- website/landingpage/src/components/Hero.js
+++ website/landingpage/src/components/Hero.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Hero = () => {
     return (
-      <div className="bg-gray-700 text-white text-center p-12">
-        <h1 className="text-4xl font-bold">Discover BowTied Members And Services</h1>
-        <p className="text-xl mt-4">Find the best resources on the internet.</p>
+        <div className="bg-gray-700 text-white text-center p-6 sm:p-8 md:p-12">
+        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">Discover BowTied Members And Services</h1>
+        <p className="text-lg sm:text-xl mt-2 sm:mt-4">Find the best resources on the internet.</p>
         {/* <div className="features">
             <h3>Features</h3>
             <ul>
